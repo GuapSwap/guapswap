3cd2b01fa97e49aa920f5f5709fdc9b2bac22838
Luca D'Angelo <ldgaetano@gmail.com>
2022-02-10 20:53:51 -0500

    Implemented proper usage of substConstants method and fixed service fee calculation to obtain an integer of type Long

--- src/main/scala/contracts/GuapSwapErgoDexSwapSellProxyContract.scala
+++ src/main/scala/contracts/GuapSwapErgoDexSwapSellProxyContract.scala
@@ -13,7 +13,7 @@ object GuapSwapErgoDexSwapSellProxyContract {
             val PK: SigmaProp
             val ErgoDexSwapSellContractTemplate: Coll[Byte]
             val GuapSwapServiceFeePercentageNum: Long = 5  
-            val GuapSwapServiceFeePercentageDen: Long = 1000
+            val GuapSwapServiceFeePercentageDenom: Long = 1000
             val GuapSwapServiceFeeContractTemplate: Coll[Byte]
             val GuapSwapMinerFee: Long
 
@@ -27,7 +27,7 @@ object GuapSwapErgoDexSwapSellProxyContract {
             // 3  => BaseAmount: Long               => 2, 17
             // 4  => DexFeePerTokenNum: Long        => 11
             // 5  => DexFeePerTokenDenom: Long      => 12
-            // 6  => MaxErgoDexMinerFee: Long       => 22
+            // 6  => MaxMinerFee: Long              => 22
             // 7  => PoolNFT: Coll[Byte]            => 8
             // 9  => NewPK: SigmaProp               => 0 (Includes the SigmaPropConstPrefixHex added to the original miner PK)
 
@@ -38,34 +38,45 @@ object GuapSwapErgoDexSwapSellProxyContract {
             val BaseAmount: Long            = getVar[Long](3).get
             val DexFeePerTokenNum: Long     = getVar[Long](4).get
             val DexFeePerTokenDenom: Long   = getVar[Long](5).get
-            val MaxErgoDexMinerFee: Long    = getVar[Long](6).get
+            val MaxMinerFee: Long           = getVar[Long](6).get
             val PoolNFT: Coll[Byte]         = getVar[Coll[Byte]](7).get
             val NewPK: SigmaProp            = getVar[SigmaProp](9).get
 
             // Replacing the ErgoDex variable values in the SwapSell template with their corresponding value from the transaction context.
-            val positions: Coll[Int] = Coll(0, 2, 8, 9, 10, 11, 12, 14, 17, 18, 22)
+            val positions_Long: Coll[Int] = Coll(2, 10, 11, 12, 14, 17, 18, 22)
+            val positions_Coll_Byte: Coll[Int] = Coll(8, 9)
+            val positions_SigmaProp: Coll[Int] = Coll(0)
 
-            val newValues: Coll[T] = Coll( // Do I need to call substConstants multiple times with different Coll[T] for the different types?
-                NewPK,
-                BaseAmount,
-                PoolNFT,
-                QuoteId,
+            val newValues_Long: Coll[Long] = Coll(
+                FeeNum,
                 MinQuoteAmount,
+                BaseAmount,
                 DexFeePerTokenNum,
                 DexFeePerTokenDenom,
-                FeeNum,
-                BaseAmount,
-                FeeNum,
-                MaxErgoDexMinerFee
+                MaxMinerFee
             )
 
-            val newErgoDexSwapSellTemplate: Coll[Byte] = substConstants[T](ErgoDexSwapSellTemplate, positions, newValues)
+            val newValues_Coll_Byte: Coll[Coll[Bytes]] = Coll(
+                PoolNFT,
+                QuoteId
+            )
+
+            val newValues_SigmaProp: Coll[SigmaProp] = Coll(
+                NewPK
+            )
+
+            // Will insert new values based on the following order: Long => Coll[Byte] => SigmaProp
+            val newErgoDexSwapSellTemplate_Long: Coll[Byte] = substConstants(ErgoDexSwapSellTemplate, positions_Long, newValues_Long)
+            val newErgoDexSwapSellTemplate_Coll_Byte: Coll[Byte] = substConstants(newErgoDexSwapSellTemplate_Long, positions_Coll_Byte, newValues_Coll_Byte)
+            val newErgoDexSwapSellTemplate_SigmaProp: Coll[Byte] = substConstants(newErgoDexSwapSellTemplate_Coll_Byte, positions_SigmaProp, newValues_SigmaProp)
+            
+            val newErgoDexSwapSellTemplate: Coll[Byte] = newErgoDexSwapSellTemplate_SigmaProp
 
             // ====== GuapSwap ErgoDex SwapSell Proxy Contract Conditions ====== //
             // Check that a valid ErgoDex SwapSell Box is an output.
             val validErgoDexSwapBox = {
-                val userSwapBox = OUTPUTS(0)
-                val validSwapBoxValue = SELF.value - ((GuapSwapServiceFeeNum / GuapSwapServiceFeeDen) * SELF.value) - GuapSwapMinerFee
+                val userSwapBox: Box = OUTPUTS(0)
+                val validSwapBoxValue: Long = SELF.value - (GuapSwapServiceFeeNum * SELF.value / GuapSwapServiceFeeDenom) - GuapSwapMinerFee
                 BaseAmount == validSwapBoxValue &&
                 userSwapBox.value == validSwapBoxValue && 
                 userSwapBox.propositionBytes == newErgoDexSwapSellTemplate
@@ -73,15 +84,15 @@ object GuapSwapErgoDexSwapSellProxyContract {
 
             // Check that a valid GuapSwap Service Fee Box is an output.
             val validGuapSwapServiceFeeBox = {
-                val serviceFeeBox = OUTPUTS(1)
-                serviceFeeBox.value == (GuapSwapServiceFeeNum / GuapSwapServiceFeeDen) * SELF.value &&
-                serviceFeeBox.propositionBytes == GuapSwapServiceFeeContract
+                val serviceFeeBox: Box = OUTPUTS(1)
+                serviceFeeBox.value == (GuapSwapServiceFeeNum * SELF.value / GuapSwapServiceFeeDenom) &&
+                serviceFeeBox.propositionBytes == GuapSwapServiceFeeContractTemplate
             }
 
             // Check that a valid Refund Box is an output if initiated by user.
             val validRefundBox = {
-                val refundBox = OUTPUTS(0)
-                refundBox.value == SELF.value - ((GuapSwapServiceFeeNum / GuapSwapServiceFeeDen) * SELF.value) - GuapSwapMinerFee &&
+                val refundBox: Box = OUTPUTS(0)
+                refundBox.value == SELF.value - (GuapSwapServiceFeeNum * SELF.value / GuapSwapServiceFeeDenom) - GuapSwapMinerFee &&
                 refundBox.propositionBytes == PK
             }
 
