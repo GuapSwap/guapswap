1dfeb26500ae6ea347c3ff51b2c4a080a4882717
Luca D'Angelo <ldgaetano@protonmail.com>
2022-03-05 01:37:49 -0500

    added protocol file

--- src/main/scala/protocol/GuapSwapTx.scala
+++ src/main/scala/protocol/GuapSwapTx.scala
@@ -1,5 +1,8 @@
 package protocol
 
+import configs.parameters.GuapSwapParameters
+import org.ergoplatform.appkit.InputBox
+
 /**
   * Class representing a GuapSwap tx, from which the tx swap sell parameters will be derived.
   *
@@ -8,5 +11,6 @@ package protocol
 case class GuapSwapTx(
     val inputs: List[InputBox],
     val outputs: List[DexPool],
+    val ratios: List[(Long, Long)],
     val parameters: GuapSwapParameters
 )
